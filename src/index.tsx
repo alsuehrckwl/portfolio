a0c520137d7d4ceece3cdd36d61d3864d97e5d97
jwkim88 <jwkim88@1thefull.com>
2019-04-10 11:13:55 +0900

    mobx init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,49 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
+import DevTools from 'mobx-react-devtools'
+
+class AppState {
+  @observable
+  public timer = 0
+
+  constructor() {
+    setInterval(() => {
+      this.timer += 1
+    },          1000)
+  }
+
+  public resetTimer() {
+    this.timer = 0
+  }
+}
+
+@observer
+class TimerView extends React.Component<{ appState: AppState }, {}> {
+  public render() {
+    return (
+      <div>
+        <button onClick={this.onReset}>
+          Seconds passed: {this.props.appState.timer}
+        </button>
+        <DevTools />
+      </div>
+    )
+  }
+
+  public onReset = () => {
+    this.props.appState.resetTimer()
+  }
+}
+
+const appState = new AppState()
+
+ReactDOM.render(<TimerView appState={appState} />, document.getElementById(
+  'root',
+) as HTMLElement)
 registerServiceWorker()
